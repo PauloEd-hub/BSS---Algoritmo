dea17b7eab67e3c6ed093b27e3e34fe5ca46a979
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-10-21 11:32:08 -0300

    Atividade 1

---
+++ atvd1Bss.js
@@ -0,0 +1,10 @@
+let number = 0
+
+while(number < 10000) {
+    number++
+    if(number % 2 == 0) {
+        console.log("É par: ", number)
+    } else{
+        console.log("É impar: ", number)
+    }
+}
